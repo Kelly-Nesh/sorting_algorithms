a6f7594bf76388ca72fec523f4d0639357a31f5e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-01-24 13:31:06 +0300

    sorting_algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort implementation
+ * @array: an array of integers
+ * @size: size of the array
+ *
+ * Return: void
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j, cnt, swap;
+
+    cnt = size;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < cnt-1; j++)
+        {
+            if (array[j] > array[j+1])
+            {
+                swap = array[j];
+                array[j] = array[j + 1];
+                array[j+1] = swap;
+                print_array(array, size);
+            }
+        }
+    cnt--;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sorting_algorithms
 c-sorting_algorithms
+0-bubble_sort.c
+0-O
+main.c
+print_array.c
+printlist.c
+README.md
+sort.h
