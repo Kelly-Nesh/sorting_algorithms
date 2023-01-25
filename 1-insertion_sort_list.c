02eccc6c292f7e1018a7fa7a23d2785c988663a3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-01-25 04:45:36 +0300

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,32 +1,52 @@
 #include "sort.h"
 
+/**
+ * node_swap - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to be  swapped.
+ */
+void node_swap(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i, *j, *swap;
+	listint_t *iter, *insert, *tmp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-	for (i = (*list)->next; i; i = i->next)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		j = i;
-		while (j->prev != NULL && j->n < j->prev->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			if (j->n == 7)
-			{
-				printf("\n\n%d\t%d\n", j->n, j->prev->n);
-			}
-			swap = j->prev;
-			if (swap->prev)
-				swap->prev->next = j;
-
-			if (j->next)
-				j->next->prev = swap;
-				
-			j->prev = swap->prev;
-			swap->prev = j;
-			swap->next = j->next;
-			j->next = swap;
-			print_list(j);
+			node_swap(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
 	}
 }
+
  
